dd2eb256ef5cf30a05bcfab3148f65b1cbd7abcc
lisawalker <lisawalker@lisas-imac>
2016-10-05 09:40:28 -0500

    Beginning stages of location

---
+++ PrincessBride/src/princessbride/PrincessBride.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package princessbride;
+
+/**
+ *
+ * @author lisawalker
+ */
+public class PrincessBride {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Game gameone = new game();
+        Location locationone = new location();
+    }
+    
+}
