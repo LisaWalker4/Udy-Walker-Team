86580fc907ae688a38ef9d21a5afe7b97f5c5620
lisawalker <lisawalker@lisas-imac>
2016-10-05 09:53:34 -0500

    Beginning stages of location

--- PrincessBride/src/princessbride/location_1.java
+++ PrincessBride/src/princessbride/location_1.java
@@ -4,14 +4,72 @@
  * and open the template in the editor.
  */
 package princessbride;
-
+import java.io.Serializable;
 /**
  *
  * @author lisawalker
  */
-class location extends Location {
+public class Location implements Serializable{
+    // class instance variables
+         private String name;
+          private double row;
+          private double column;
+          private double visited;
+          private double description;
+          private double addItem;
+          
+          
 
-    public location() {
+    public double getAddItem() {
+        return addItem;
     }
-    
+
+    public void setAddItem(double addItem) {
+        this.addItem = addItem;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getRow() {
+        return row;
+    }
+
+    public void setRow(double row) {
+        this.row = row;
+    }
+
+    public double getColumn() {
+        return column;
+    }
+
+    public void setColumn(double column) {
+        this.column = column;
+    }
+
+    public double getVisited() {
+        return visited;
+    }
+
+    public void setVisited(double visited) {
+        this.visited = visited;
+    }
+
+    public double getDescription() {
+        return description;
+    }
+
+    public void setDescription(double description) {
+        this.description = description;
+    }
+
+           
+                            
+            
 }
